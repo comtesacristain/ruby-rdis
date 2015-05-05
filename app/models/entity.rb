711a20948aeec8e3cd7e1f1ea4fca68b53917a62
Michael Sexton <mick.sexton@gmail.com>
2015-05-05 13:21:07 +1000

    Changed to Rails 4

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,7 +2,7 @@ class Entity < ActiveRecord::Base
 
 
 	self.table_name = "a.entities"
-  set_primary_key :eno
+  self.primary_ket = :eno
 	set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
 
 end
