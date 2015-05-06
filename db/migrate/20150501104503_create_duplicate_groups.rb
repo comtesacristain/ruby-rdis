93cad477a11d9bf83bf735fe661b76ecab34f944
Michael Sexton <mick.sexton@gmail.com>
2015-05-06 22:04:20 +1000

    Tidy up

--- db/migrate/20150501104503_create_duplicate_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateDuplicateGroups < ActiveRecord::Migration
-  def change
-    create_table :duplicate_groups do |t|
-      t.string :kind
-
-      t.timestamps null: false
-    end
-  end
-end
--- test/models/duplicate_group_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DuplicateGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
