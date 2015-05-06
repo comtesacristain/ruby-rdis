d8a3a2ae60122dc7013a60cbed3b20377b70241b
Michael Sexton <mick.sexton@gmail.com>
2015-05-06 21:28:57 +1000

    Changed duplicate group to duplicate

--- app/helpers/duplicates_helper.rb
+++ app/helpers/duplicates_helper.rb
@@ -1,2 +1,28 @@
 module DuplicatesHelper
+
+	def print_wells(well)
+		if well.nil?
+			return content_tag :td, 'NO WELL DATA', :colspan=>9, :class=>:nowell
+		else
+
+			ary = [well.welltype,well.operator,well.purpose,well.status,well.classification,well.start_date, well.completion_date, well.originator, well.total_depth]
+			ary.collect do |i|
+				content_tag(:td,i)
+			end.join.html_safe
+			
+		end
+
+
+	end
+
+	def get_row_class(action_status)
+		case action_status
+		when "DELETE"
+			return :delete
+		when "KEEP"
+			return :keep
+		when nil
+			return :unknown
+		end
+	end 
 end
