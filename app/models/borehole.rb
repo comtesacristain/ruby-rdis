3d560ebeb03773e7782bb509eef470bfd7ef8809
Michael Sexton <mick.sexton@gmail.com>
2015-05-07 01:39:20 +1000

    Added handler relationship

--- app/models/borehole.rb
+++ app/models/borehole.rb
@@ -4,6 +4,8 @@ class Borehole < ActiveRecord::Base
 	has_many :borehole_duplicates
 	has_many :duplicates, :through => :borehole_duplicates
   
+  has_one :handler
+  
   def entity
     return Entity.find(self.eno)
   end
