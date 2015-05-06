781fee6250c09f32f53035587142b29ca6639073
Michael Sexton <mick.sexton@gmail.com>
2015-05-06 21:32:22 +1000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :duplicates
+  resources :boreholes
+  resources :duplicate_groups
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
