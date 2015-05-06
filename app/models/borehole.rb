702b84a731bcce91d7652fce44781af910be7142
Michael Sexton <mick.sexton@gmail.com>
2015-05-07 01:37:05 +1000

    Cleaned up text

--- app/models/borehole.rb
+++ app/models/borehole.rb
@@ -1,9 +1,9 @@
 class Borehole < ActiveRecord::Base
 	establish_connection :local
 		
-
-		has_many :borehole_duplicates
+	has_many :borehole_duplicates
 	has_many :duplicates, :through => :borehole_duplicates
+  
   def entity
     return Entity.find(self.eno)
   end
