274f468c460010a1f5bcb68dc26047fd4b7d72fe
Michael Sexton <mick.sexton@gmail.com>
2015-05-11 14:15:08 +1000

    Link to boreholes from duplicates

--- app/helpers/boreholes_helper.rb
+++ app/helpers/boreholes_helper.rb
@@ -1,2 +1,17 @@
 module BoreholesHelper
+
+  def transfer_link(number)
+    
+    case number
+    when nil
+      return number
+    else
+      borehole = try  Borehole.find(number.to_i)
+      unless borehole.nil?
+        return link_to number, borehole
+      else
+        return number
+      end
+    end
+  end
 end
