3aa1634df0e38a105620b960882a638f18b37a65
Michael Sexton <mick.sexton@gmail.com>
2015-05-11 10:54:59 +1000

    Maybe this will work

--- app/models/duplicate.rb
+++ app/models/duplicate.rb
@@ -4,4 +4,6 @@ class Duplicate < ActiveRecord::Base
 	has_many :borehole_duplicates
 	has_many :boreholes, :through => :borehole_duplicates
   has_many :handlers, :through => :boreholes
+  
+  accepts_nested_attributes_for :handlers
 end
