5103dd8d4aec6fc2e47dae312333eb56cd7a8472
Michael Sexton <mick.sexton@gmail.com>
2015-05-11 14:42:00 +1000

    Added wells

--- app/helpers/boreholes_helper.rb
+++ app/helpers/boreholes_helper.rb
@@ -14,4 +14,27 @@ module BoreholesHelper
       end
     end
   end
+  
+	def print_wells(well)
+		if well.nil?
+			return content_tag :td, 'NO WELL DATA', :colspan=>9, :class=>:nowell
+		else
+			ary = get_wells_row(well)
+			ary.collect do |i|
+				content_tag(:td,i)
+			end.join.html_safe
+		end
+	end
+  
+	def get_wells_row(well)
+    if well.nil?
+      row=  Array.new(9)
+		else
+			row = [well.welltype,well.operator,well.purpose,well.status,well.classification,well.start_date, well.completion_date, well.originator, well.total_depth]
+			
+		end
+    return row
+	end
+  
+  
 end
