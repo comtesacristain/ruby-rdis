5714f3ef7b9d10fb0a1a17623a4e9af450b938c4
Michael Sexton <mick.sexton@gmail.com>
2015-05-16 00:59:52 +1000

    Migrate properly bro

--- db/migrate/20150513132427_add_indices_to_boreholes.rb
+++ db/migrate/20150513132427_add_indices_to_boreholes.rb
@@ -1,4 +1,7 @@
 class AddIndicesToBoreholes < ActiveRecord::Migration
   def change
+    add_index :boreholes, :eno
+    add_index :boreholes, :entity_type
+    add_index :boreholes, :entityid
   end
 end
--- db/migrate/20150513132447_add_indices_to_handlers.rb
+++ db/migrate/20150513132447_add_indices_to_handlers.rb
@@ -1,5 +1,13 @@
 class AddIndicesToHandlers < ActiveRecord::Migration
   def change
-    add_index 
+    add_index :handlers, :auto_remedation
+    add_index :handlers, :or_status
+    add_index :handlers, :or_transfer
+    add_index :handlers, [:borehole_id,:or_status]
+    add_index :boreholes, :eno
+    add_index :boreholes, :entity_type
+    add_index :boreholes, :entityid
+    
+    
   end
 end
