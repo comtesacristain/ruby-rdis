106be39da0e6b55ed0ba8843233693122f6c759c
Michael Sexton <mick.sexton@gmail.com>
2015-05-16 10:56:54 +1000

    Added defaults to two columns

--- db/migrate/20150516005418_change_columns_in_handlers.rb
+++ db/migrate/20150516005418_change_columns_in_handlers.rb
@@ -1,4 +1,6 @@
 class ChangeColumnsInHandlers < ActiveRecord::Migration
   def change
+    change_column :handlers, :auto_remediation, :string, default:"NONE"
+    change_column :handlers, :or_status, :string, default:"undetermined"
   end
 end
