78f5c2941983f7040126dc8fe9c545a8f93f70c3
Michael Sexton <mick.sexton@gmail.com>
2015-05-18 00:39:25 +1000

    Added columns to wells

--- db/migrate/20150517141956_add_columns_to_borehole_wells.rb
+++ db/migrate/20150517141956_add_columns_to_borehole_wells.rb
@@ -1,4 +1,27 @@
 class AddColumnsToBoreholeWells < ActiveRecord::Migration
   def change
+    add_column :borehole_wells, :eno, :integer
+    add_column :borehole_wells, :well_type, :string
+    add_column :borehole_wells, :purpose, :string
+    add_column :borehole_wells, :on_off, :string
+    add_column :borehole_wells, :title, :string
+    add_column :borehole_wells, :classification, :string
+    add_column :borehole_wells, :status, :string
+    add_column :borehole_wells, :ground_elev, :float
+    add_column :borehole_wells, :operator, :string
+    add_column :borehole_wells, :uno, :string
+    add_column :borehole_wells, :start_date, :date
+    add_column :borehole_wells, :completion_date, :date
+    add_column :borehole_wells, :comments, :string
+    add_column :borehole_wells, :total_depth, :float
+    add_column :borehole_wells, :originator, :string
+    add_column :borehole_wells, :origno, :integer
+    
+    
+         
+    
+    add_column :borehole_wells, :borehole, :reference
+    
+    add_index :borehole_wells, :eno
   end
 end
