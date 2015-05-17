328e3fe383c671703ef6e9bfc7d65f9d29b2ce24
Michael Sexton <mick.sexton@gmail.com>
2015-05-18 09:54:33 +1000

    Added migrations for minerals and entity attributes

--- db/migrate/20150517142014_add_columns_to_borehole_mineral_attributes.rb
+++ db/migrate/20150517142014_add_columns_to_borehole_mineral_attributes.rb
@@ -1,4 +1,17 @@
 class AddColumnsToBoreholeMineralAttributes < ActiveRecord::Migration
   def change
+   
+    add_column :borehole_mineral_attributes, :attribno, :integer
+    add_column :borehole_mineral_attributes, :eno, :integer
+    add_column :borehole_mineral_attributes, :attr, :string #Attribute
+    add_column :borehole_mineral_attributes, :num_value, :float #Attribute
+    add_column :borehole_mineral_attributes, :text_value, :string #Attribute
+    add_column :borehole_mineral_attributes, :date_value, :date #Attribute
+    
+         
+    
+    add_reference :borehole_mineral_attributes, :borehole
+    
+    add_index :borehole_mineral_attributes, :eno
   end
 end
