1f8c16e8898b28b6561e8b65640a29689eda7f86
Michael Sexton <mick.sexton@gmail.com>
2015-05-18 09:08:27 +1000

    Changes to borehole_samples

--- db/migrate/20150517141938_add_columns_to_borehole_samples.rb
+++ db/migrate/20150517141938_add_columns_to_borehole_samples.rb
@@ -4,12 +4,13 @@ class AddColumnsToBoreholeSamples < ActiveRecord::Migration
     add_column :borehole_samples, :eno, :integer
     add_column :borehole_samples, :sampleid, :string
     add_column :borehole_samples, :sample_type, :string
-    add_column :borehole_samples, :total_depth, :float
+    add_column :borehole_samples, :top_depth, :float
     add_column :borehole_samples, :base_depth, :float
     add_column :borehole_samples, :parent, :integer
     add_column :borehole_samples, :originator, :string
-    add_column :borehole_samples, :acquire_date, :date
+    add_column :borehole_samples, :acquiredate, :date
     add_column :borehole_samples, :geom_original, :string
+
     
     add_reference :borehole_samples, :borehole
     
