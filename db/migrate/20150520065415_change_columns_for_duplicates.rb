779b43639aa5c8bf4a2ccdc90dd35010af713182
Michael Sexton <mick.sexton@gmail.com>
2015-05-20 17:33:12 +1000

    Have to have a string you dummy

--- db/migrate/20150520065415_change_columns_for_duplicates.rb
+++ db/migrate/20150520065415_change_columns_for_duplicates.rb
@@ -2,7 +2,7 @@ class ChangeColumnsForDuplicates < ActiveRecord::Migration
   def change
     rename_column :duplicates, :qaed, :manual_remediation
     rename_column :duplicates, :has_remediation, :auto_remediation
-    add_column :duplicates, :auto_approved
+    add_column :duplicates, :auto_approved, :string
     
     
 
