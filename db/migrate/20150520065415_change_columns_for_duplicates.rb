5d0f9af24982cb1d02b11c02b22e48490d75a4f6
Michael Sexton <mick.sexton@gmail.com>
2015-05-20 17:25:22 +1000

    Added and modified columns in Duplicate

--- db/migrate/20150520065415_change_columns_for_duplicates.rb
+++ db/migrate/20150520065415_change_columns_for_duplicates.rb
@@ -1,4 +1,10 @@
 class ChangeColumnsForDuplicates < ActiveRecord::Migration
   def change
+    rename_column :duplicates, :qaed, :manual_remediation
+    rename_column :duplicates, :has_remediation, :auto_remediation
+    add_column :duplicates, :auto_approved
+    
+    change column :duplicates, :manual_remediation, :string, default:"N" 
+
   end
 end
