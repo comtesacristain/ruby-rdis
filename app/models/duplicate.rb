02dd0cabb19fb5c5e4aa8acbdc2f7a08cb34c429
Michael Sexton <mick.sexton@gmail.com>
2015-05-22 14:15:08 +1000

    Removed aliases

--- app/models/duplicate.rb
+++ app/models/duplicate.rb
@@ -7,23 +7,5 @@ class Duplicate < ActiveRecord::Base
   
   accepts_nested_attributes_for :handlers
 
-  # DELETE the following when done
-  
-
-  def has_remediation
-    if Rails.env=="development"
-      return @auto_remediation
-    else
-      return @has_remediation
-    end
-  end
-  
-  def manual_remediation
-    if Rails.env=="development"
-      return @qaed
-    else
-      return @manual_remediation
-    end
-  end
 
 end
