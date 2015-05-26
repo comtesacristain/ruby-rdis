28be0978c1d52047fe71fd63e583185b4895556f
Michael Sexton <mick.sexton@gmail.com>
2015-05-26 17:35:54 +1000

    A duplicate is allowed to have many wells

--- app/models/duplicate.rb
+++ app/models/duplicate.rb
@@ -5,6 +5,8 @@ class Duplicate < ActiveRecord::Base
 	has_many :boreholes, :through => :borehole_duplicates
   has_many :handlers, :through => :boreholes
   
+  has_many :wells, :through => :boreholes, :class_name => "BoreholeWell"
+  
   accepts_nested_attributes_for :handlers
 
 
