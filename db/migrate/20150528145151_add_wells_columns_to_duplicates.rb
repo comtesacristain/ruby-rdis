2b5619dbd829b4cb9d9838bcc8aa3ba730b30cdc
Michael Sexton <mick.sexton@gmail.com>
2015-05-29 00:56:40 +1000

    Adding columns to duplicates

--- db/migrate/20150528145151_add_wells_columns_to_duplicates.rb
+++ db/migrate/20150528145151_add_wells_columns_to_duplicates.rb
@@ -1,4 +1,17 @@
 class AddWellsColumnsToDuplicates < ActiveRecord::Migration
   def change
+    add_column :duplicates, :welltype, :integer
+    add_column :duplicates, :purpose, :integer
+    add_column :duplicates, :title, :integer
+    add_column :duplicates, :classification, :integer
+    add_column :duplicates, :status, :integer
+    add_column :duplicates, :ground_elev, :integer
+    add_column :duplicates, :operator, :integer
+    add_column :duplicates, :uno, :integer
+    add_column :duplicates, :start_date, :integer
+    add_column :duplicates, :completion_date, :integer
+    add_column :duplicates, :total_depth, :integer
+    add_column :duplicates, :originator, :integer
+    add_column :duplicates, :origno, :integer
   end
 end
