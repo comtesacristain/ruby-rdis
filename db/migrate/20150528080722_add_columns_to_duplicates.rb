b16efdd374c0b63128b2b61771df880d5f239faa
Michael Sexton <mick.sexton@gmail.com>
2015-05-28 23:12:47 +1000

    Adding columns

--- db/migrate/20150528080722_add_columns_to_duplicates.rb
+++ db/migrate/20150528080722_add_columns_to_duplicates.rb
@@ -1,4 +1,8 @@
 class AddColumnsToDuplicates < ActiveRecord::Migration
   def change
+    add_column :duplicates, :eid_qualifier, :integer
+    add_column :duplicates, :remark, :integer
+    add_column :duplicates, :access_code, :integer
+    add_column :duplicates, :acquisition_methodno, :integer
   end
 end
