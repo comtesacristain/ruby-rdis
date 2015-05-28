d22b0d0a97a21424ab1aed101d2a137f44a6d88e
Michael Sexton <mick.sexton@gmail.com>
2015-05-28 14:48:02 +1000

    Update duplicate model to determine what should be kept

--- app/models/duplicate.rb
+++ app/models/duplicate.rb
@@ -10,4 +10,20 @@ class Duplicate < ActiveRecord::Base
   accepts_nested_attributes_for :handlers
 
 
+  def pick_kept
+    borehole = self.boreholes.includes(:handler).find_by(handlers:{auto_remediation:"KEEP"})
+    self.keep = borehole.eno
+  end
+
+  def pick_geom_original
+    boreholes = self.boreholes.select{|b| !b.z.nil?}
+    if boreholes.size == 1
+      self.geom_original = boreholes.first.eno
+    else
+      return self.geom_original = self.keep
+    end
+    self.save
+  end
+
+
 end
