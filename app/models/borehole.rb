49076516d9a6a9ec856774bc27d2fe5d6e3babe4
Michael Sexton <mick.sexton@gmail.com>
2015-07-26 21:58:12 +1000

    Added borehole relationships

--- app/models/borehole.rb
+++ app/models/borehole.rb
@@ -8,6 +8,8 @@ class Borehole < ActiveRecord::Base
   
   has_one :well, :class_name => "BoreholeWell"
   has_many :samples, :class_name => "BoreholeSample"
+  has_many :entity_attributes, :class_name => "BoreholeEntityAttributes"
+  has_many :mineral_attributes, :class_name => "BoreholeMineralAttributes"
   
   def entity
     return Entity.find(self.eno)
