47a1e81a218f04185a15efbc1872ee874c912980
Michael Sexton <mick.sexton@gmail.com>
2015-07-31 14:59:37 +1000

    Removed tab

--- app/models/well.rb
+++ app/models/well.rb
@@ -2,7 +2,7 @@ class Well < ActiveRecord::Base
   establish_connection "oracle_#{Rails.env}"
 
 	self.table_name = "npm.wells"
-    self.primary_key = :eno
+  self.primary_key = :eno
 	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
   has_many :well_confids, :foreign_key => :eno
 	belongs_to :entity
