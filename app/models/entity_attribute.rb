61419ba51a2d6ca7d10032f48af3ad4552acac3c
Michael Sexton <mick.sexton@gmail.com>
2015-07-31 15:01:42 +1000

    Assignment

--- app/models/entity_attribute.rb
+++ app/models/entity_attribute.rb
@@ -1,8 +1,8 @@
 class EntityAttribute < ActiveRecord::Base
   establish_connection "oracle_#{Rails.env}"
 
-  self.table_name "mgd.entity_attribs"
-  self.primary_key :eno
+  self.table_name = "mgd.entity_attribs"
+  self.primary_key = :eno
   set_date_columns :entrydate, :qadate, :confid_until, :lastupdate
 
   class << self
