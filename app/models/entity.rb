3bb9cfe3665c7436b8cc7b567b80b231d7581f5c
Michael Sexton <mick.sexton@gmail.com>
2015-07-31 15:01:33 +1000

    Removed tab

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,8 +2,9 @@ class Entity < ActiveRecord::Base
   establish_connection "oracle_#{Rails.env}"
 
 	self.table_name = "a.entities"
-    self.primary_key = :eno
-	set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
+  self.primary_key = :eno
+	
+  set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
 	has_one :well, :foreign_key =>:eno
 	has_many :samples, :foreign_key => :eno
   has_many :entity_attributes, :foreign_key => :eno
