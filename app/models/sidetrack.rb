54f291a09451d9a808ce739fe97626e61343651f
Michael Sexton <mick.sexton@gmail.com>
2015-08-03 15:32:34 +1000

    Added sidetrack relation

--- app/models/sidetrack.rb
+++ app/models/sidetrack.rb
@@ -1,8 +1,8 @@
-class Remarkw < ActiveRecord::Base
+class Sidetrack < ActiveRecord::Base
   establish_connection "oracle_#{Rails.env}"
   
 	self.table_name = "npd.npd_sidetrack"
-  self.primary_key = :eno
+  self.primary_key = :eno
 	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
 	belongs_to :well
 end
