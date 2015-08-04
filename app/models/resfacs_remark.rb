df37b87795bde90e151379b106c52448c809fd04
Michael Sexton <mick.sexton@gmail.com>
2015-08-04 17:23:12 +1000

    Change to model, with correct primary key

--- app/models/resfacs_remark.rb
+++ app/models/resfacs_remark.rb
@@ -2,7 +2,7 @@ class ResfacsRemark < ActiveRecord::Base
   establish_connection "oracle_#{Rails.env}"
   
 	self.table_name = "resfacs.rfs_rmks"
-  self.primary_key = :eno
+  self.primary_key = :rfs_rmksno
 	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
-	belongs_to :well
+	belongs_to :entity
 end
