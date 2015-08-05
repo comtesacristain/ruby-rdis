c687a6cdd0b8c4eff9501328d1a4d0aed4b2e665
Michael Sexton <mick.sexton@gmail.com>
2015-08-05 15:03:59 +1000

    Incorrect spelling

--- app/models/poperm_two.rb
+++
@@ -1,2 +0,0 @@
-class PopermTwo < ActiveRecord::Base
-end
---
+++ app/models/porperm_two.rb
@@ -0,0 +1,8 @@
+class PorpermTwo < ActiveRecord::Base
+  establish_connection "oracle_#{Rails.env}"
+  
+	self.table_name = "npd.npd_porperm2"
+  self.primary_key = :eno
+	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
+	belongs_to :entity
+end
