adb25600425c3a4ba772f6b5ef64932418cb1977
Michael Sexton <mick.sexton@gmail.com>
2015-08-05 21:06:51 +1000

    Fix

--- app/controllers/borehole_mineral_attributes_controller.rb
+++ app/controllers/borehole_mineral_attributes_controller.rb
@@ -1,7 +1,7 @@
 class BoreholeMineralAttributesController < ApplicationController
   def index
     @columns = BoreholeMineralAttribute.column_names
-    @borehole_mineral_attributes = BoreholeMineralAttribute.all
+    @mineral_attributes = BoreholeMineralAttribute.all
     respond_to do |format|
       format.xlsx
     end
