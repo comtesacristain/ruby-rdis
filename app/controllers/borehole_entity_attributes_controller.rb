0c6411ecbf7593c7eae975ab2571e99888130819
Michael Sexton <mick.sexton@gmail.com>
2015-08-05 21:06:32 +1000

    Fix

--- app/controllers/borehole_entity_attributes_controller.rb
+++ app/controllers/borehole_entity_attributes_controller.rb
@@ -1,7 +1,7 @@
 class BoreholeEntityAttributesController < ApplicationController
   def index
     @columns = BoreholeEntityAttribute.column_names
-    @borehole_entity_attributes = BoreholeEntityAttribute.all
+    @entity_attributes = BoreholeEntityAttribute.all
     respond_to do |format|
       format.xlsx
     end
