2b29e86053c4bb93607b6f32cd3555410306fe78
Michael Sexton <mick.sexton@gmail.com>
2015-08-05 18:54:46 +1000

    Added deviant data

---
+++ app/models/dir_survey.rb
@@ -0,0 +1,9 @@
+class DirSurvey < ActiveRecord::Base
+  establish_connection "oracle_#{Rails.env}"
+  
+	self.table_name = "deviant.dir_srvy"
+  self.primary_key = :eno
+	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
+	belongs_to :entity
+
+end
