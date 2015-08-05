7190b35adfc920ae5042d085244de245916190c5
Michael Sexton <mick.sexton@gmail.com>
2015-08-05 15:52:46 +1000

    Try this

--- app/models/resultw.rb
+++ app/models/resultw.rb
@@ -5,4 +5,12 @@ class Resultw < ActiveRecord::Base
   self.primary_key = :eno
 	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
 	belongs_to :entity
+  
+  class << self
+    def instance_method_already_implemented?(method_name)
+      return true if method_name =~ /type/
+      super
+    end
+  end
+  
 end
