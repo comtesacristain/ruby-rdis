d4afad4972e4eb0d50befa969d2543f5d79b91fd
Michael Sexton <mick.sexton@gmail.com>
2015-08-05 15:54:41 +1000

    No inheritance column

--- app/models/resultw.rb
+++ app/models/resultw.rb
@@ -6,11 +6,6 @@ class Resultw < ActiveRecord::Base
 	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
 	belongs_to :entity
   
-  class << self
-    def instance_method_already_implemented?(method_name)
-      return true if method_name =~ /type/
-      super
-    end
-  end
+  self.inheritance_column = nil 
   
 end
