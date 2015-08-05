c1566131f25b5a9fa0c199bf6e81422cc333c179
Michael Sexton <mick.sexton@gmail.com>
2015-08-05 18:05:06 +1000

    Foreign key is always eno

--- app/models/borehole.rb
+++ app/models/borehole.rb
@@ -6,10 +6,10 @@ class Borehole < ActiveRecord::Base
   
   has_one :handler
   
-  has_one :well, :class_name => "BoreholeWell"
-  has_many :samples, :class_name => "BoreholeSample"
-  has_many :entity_attributes, :class_name => "BoreholeEntityAttributes"
-  has_many :mineral_attributes, :class_name => "BoreholeMineralAttributes"
+  has_one :well, :class_name => "BoreholeWell", :foreign_key => :eno
+  has_many :samples, :class_name => "BoreholeSample", :foreign_key => :eno
+  has_many :entity_attributes, :class_name => "BoreholeEntityAttributes", :foreign_key => :eno
+  has_many :mineral_attributes, :class_name => "BoreholeMineralAttributes", :foreign_key => :eno
   
   def entity
     return Entity.find(self.eno)
