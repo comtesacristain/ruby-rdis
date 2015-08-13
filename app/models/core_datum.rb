4ddd2f8c2b217e2d78a47c122bc0dc6bc0c015a1
Michael Sexton <mick.sexton@gmail.com>
2015-08-13 16:26:14 +1000

    No inheritance column

--- app/models/core_datum.rb
+++ app/models/core_datum.rb
@@ -5,4 +5,6 @@ class CoreDatum < ActiveRecord::Base
   self.primary_key = :eno
 	#set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
 	belongs_to :entity
+  
+  self.inheritance_column = nil 
 end
