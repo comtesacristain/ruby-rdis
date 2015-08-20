73bf3c2658a762ad92fc20f55067fb6581bc173a
Michael Sexton <mick.sexton@gmail.com>
2015-08-20 16:07:29 +1000

    Set sequence name

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,6 +3,8 @@ class Entity < ActiveRecord::Base
 
 	self.table_name = "a.entities"
   self.primary_key = :eno
+  
+  self.sequence_name="a.seq_entities_eno"
 	
   set_date_columns :entrydate, :qadate, :lastupdate, :effective_date, :acquisition_date, :expiry_date
 	has_one :well, :foreign_key =>:eno
